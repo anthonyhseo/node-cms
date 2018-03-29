9dc443e32a077817cd946249778aa2a09d5c01e0
unknown <anthonyhseo@gmail.com>
2018-03-28 21:40:19 -0700

    made changes to database definition for deployment

--- config/database.js
+++ config/database.js
@@ -1,3 +1,8 @@
 module.exports = {
-  mongoDbUrl: 'mongodb://localhost:27017/cms'
-}
+  mongoDbUrl:
+    process.env.NODE_ENV === 'production'
+      ? `mongodb://${process.env.MLAB_USER}:${
+          process.env.MLAB_PASSWORD
+        }@ds127139.mlab.com:27139/node-cms`
+      : 'mongodb://localhost:27017/cms'
+};
